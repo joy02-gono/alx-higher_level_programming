de8e1237f85df7a334d73d66a86cfc0fc472c099
joy02-gono <rejoicegono@gmail.com>
2022-11-28 20:24:18 +0000

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
-* check_cycle - Checks if a singly linked list has a cycle in it
-* @list: list to check
-* Return: 0 if there is no cyrcle, 1 if there is a cycle
-*/
+ * check_cycle - Checks if a singly linked list has a cycle in it
+ * @list: list to check
+ * Return: 0 if there is no cyrcle, 1 if there is a cycle
+ */
 
 int check_cycle(listint_t *list)
-{	
+{
 listint_t *first, *second;
-	
+
 first = second = list;
 
 while (first && second && second->next)
