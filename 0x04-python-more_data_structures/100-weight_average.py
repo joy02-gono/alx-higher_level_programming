ecc5591d95e2d1e4480ffca68e9ad90742ef3251
rejoice <rejoicegono@gmail.com>
2022-12-07 12:39:15 -0800

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if len(my_list) == 0:
-        return 0
+weight_average = __import__('100-weight_average').weight_average
 
-    return sum([mul(x[0], x[1]) for x in my_list]) / sum(x[1] for x in my_list)
-
-def mul(x, y):
-    return x * y
+my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+result = weight_average(my_list)
+print("Average: {:0.2f}".format(result))
 
