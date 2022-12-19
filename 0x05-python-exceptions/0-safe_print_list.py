0f8073d783609985ad2592038789dcc368087949
rejoice <rejoicegono@gmail.com>
2022-12-19 14:01:17 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -14,6 +14,5 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             ret += 1
         except IndexError:
-            break
         print("")
         return (ret)
